2c6455f64c7950c90fd2f632dd07b75ed3919745
Andrea Monti <Andrea.Monti@vhit-weifu.com>
2026-01-22 10:47:05 +0100

    git actions

--- utExecutionAndResults/utResults/BuildTblResults/test/dependencies/BuildTbl.d
+++
@@ -1,3 +0,0 @@
-utExecutionAndResults/utUnderTest/build/gcov/out/test_BuildTbl/BuildTbl.o: \
- utExecutionAndResults/utUnderTest/src/BuildTbl.c \
- utExecutionAndResults/utUnderTest/src/BuildTbl.h
--- utExecutionAndResults/utResults/BuildTblResults/test/dependencies/unity.d
+++
@@ -1,4 +0,0 @@
-utExecutionAndResults/utUnderTest/build/gcov/out/test_BuildTbl/unity.o: \
- utExecutionAndResults/utUnderTest/build/vendor/unity/src/unity.c \
- utExecutionAndResults/utUnderTest/build/vendor/unity/src/unity.h \
- utExecutionAndResults/utUnderTest/build/vendor/unity/src/unity_internals.h
